42fcd3f1c825e2e2fc46d50606b1b23f6f1dc15c
Jolyon Russ <jolyon.russ@telegraph.co.uk>
2015-11-16 16:24:09 +0000

    Removed duplicate plugins declaration

--- index.js
+++ index.js
@@ -172,27 +172,6 @@ module.exports = function (opts) {
       )
     }
 
-    // minify in production
-    config.plugins.push(
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.OccurenceOrderPlugin(true),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        },
-        output: {
-          comments: false
-        },
-        sourceMap: false
-      }),
-      new ExtractTextPlugin(config.output.cssFilename, {
-        allChunks: true
-      }),
-      new webpack.DefinePlugin({
-        'process.env': {NODE_ENV: JSON.stringify('production')}
-      })
-    )
-
     config.module.loaders.push(
       {
         test: /\.css$/,
